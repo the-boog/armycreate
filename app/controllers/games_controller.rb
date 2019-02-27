b349b0ce48127256a03b77500b8f4a65a384df75
Joshua Larsen <larsen.bass@gmail.com>
2019-02-27 16:48:02 -0700

    add html

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,6 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :edit, :update, :destroy]
+
   def index
     @games = Game.all
   end
@@ -7,17 +9,48 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
+    render "form"
   end
 
   def create
+    @game = Game.new(game_params)
+    if @game.save
+      redirect_to games_path
+    else
+      render :new
+    end
   end
 
   def edit
+    render "form"
   end
 
   def update
+    @game = Game.update(game_params)
+    if @game.update
+      redirect_to games_path
+    else
+      render :new
+    end
   end
 
   def destroy
+    @game.destroy
+    redirect_to games_path
+  end
  
+  private
+  def game_params
+    params.require(:game).permit(:name)
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+  
+
+
 end
+
+
