a24b70de7968a42369ef4dcafbbdc3d79e977986
Chris Larsen <c.r.larsen@hotmail.com>
2019-02-27 16:40:00 -0700

    form create

--- app/controllers/races_controller.rb
+++ app/controllers/races_controller.rb
@@ -1,13 +1,49 @@
 class RacesController < ApplicationController
+  before_action :get_race, only: [:new, :edit, :show, :destroy]
+
   def index
+    @races = Race.all
   end
 
   def new
+    @race = Race.new
+    render "form"
+  end
+
+  def create
+    @race = Race.new(race_params)
+    if @race.save
+      redirect_to races_path
+    else
+    render :new
+    end
   end
 
   def edit
+    render "form"
+  end
+
+  def update
+    if @race.update(race_params)
+      redirect_to races_path
+    else
+      render :edit
+    end
   end
 
   def show
   end
+
+  def destroy
+    @race.destroy
+    redirect_to races_path
+
+  private
+    def race_params
+      params.require(:race).permit(:name)
+    end
+
+    def get_race
+      @race = Race.find(params[:id])
+    end
 end
