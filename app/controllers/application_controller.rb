0e2fe8aac4399ddb86a525dfe9472b91a3f0aa06
Chris Larsen <c.r.larsen@hotmail.com>
2019-02-28 11:01:04 -0700

    move login page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  def main
+    render 'layouts/main'
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root "games#index"
-  
+
   devise_for :users
 
   resources :games do
